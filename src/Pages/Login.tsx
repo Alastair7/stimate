2426215965631b2b1c66448fac587a1ab3fb9224
Alastair7 <alexandru.ban.drmer@gmail.com>
2024-09-17 22:08:54 +0200

    STIMATE: add session storage nickname

--- src/Pages/Login.tsx
+++ src/Pages/Login.tsx
@@ -1,23 +1,45 @@
+import { useCallback, useEffect, useState } from "react";
+
 const Login = () => {
+  const [nickname, setNickname] = useState<string>("");
+  let test = "";
+
+  const onClickNickname = () => {
+    window.sessionStorage.setItem("nickname", nickname ?? "");
+  };
+  const onChangeNickname = useCallback(
+    (event: React.ChangeEvent<HTMLInputElement>) => {
+      setNickname(event.currentTarget.value);
+    },
+    [nickname]
+  );
+
+  useEffect(() => {
+    const value = sessionStorage.getItem("nickname");
+    setNickname(value ?? "");
+  }, []);
+
   return (
     <div className="min-h-screen bg-dark_space">
-      <header className="flex flex-col items-center gap-2">
-        <h1 className="text-6xl text-gray-200">Stimate</h1>
+      <header className="flex flex-col items-center gap-2 mb-20">
+        <h1 className="text-8xl text-gray-200">Stimate</h1>
         <h2 className="text-4xl text-gray-300">
           Estimate with your teammates for FREE
         </h2>
       </header>
       <div className="flex justify-center items-center mt-7">
-        <form className="flex gap-4">
-          <div className="bg-transparent border-b border-white">
-            <input
-              className="bg-transparent border-none border-teal-500 text-gray-100 py-2 focus:outline-none"
-              type="text"
-              placeholder="Nickname"
-            />
-            <button className="ml-3 bg-sky_blue_100">Set</button>
-          </div>
-        </form>
+        <div className="bg-transparent border-b border-white">
+          <input
+            className="bg-transparent border-none border-teal-500 text-gray-100 py-2 focus:outline-none"
+            type="text"
+            value={nickname}
+            onChange={onChangeNickname}
+            placeholder="Nickname"
+          />
+          <button onClick={onClickNickname} className="ml-3 bg-sky_blue_100">
+            Set
+          </button>
+        </div>
       </div>
       <div className="flex justify-center items-center gap-2 mt-10">
         <button className="bg-cyan-200">Create Session</button>
