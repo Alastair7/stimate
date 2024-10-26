0febfedd7a1e53b9a2a6281e47aac8aedc10d066
Alastair7 <alexandru.ban.drmer@gmail.com>
2024-10-26 13:03:34 +0200

    STIMATE: add CSS to UsernameForm component

--- stimate-client/src/Features/UsernameForm/UsernameForm.tsx
+++ stimate-client/src/Features/UsernameForm/UsernameForm.tsx
@@ -1,5 +1,6 @@
 import { Button, Form, FormProps, Input } from "antd";
 import { setUsername } from "./domain";
+import "./usernameForm.css";
 type FieldType = {
   username: string;
 };
@@ -16,28 +17,31 @@ const UsernameForm = ({ setShowUsernameForm }: Props) => {
   };
 
   return (
-    <Form
-      name="basic"
-      autoComplete="off"
-      labelCol={{ span: 8 }}
-      wrapperCol={{ span: 16 }}
-      onFinish={onFinish}
-      style={{ maxWidth: 600 }}
-    >
-      <Form.Item<FieldType>
-        label="Username"
-        name="username"
-        rules={[{ required: true, message: "You need a username." }]}
+    <div className="username_form__wrapper">
+      <h1>Hey Stranger, what's your name?</h1>
+      <Form
+        name="basic"
+        autoComplete="off"
+        labelCol={{ span: 8 }}
+        wrapperCol={{ span: 16 }}
+        onFinish={onFinish}
+        style={{ maxWidth: 600 }}
       >
-        <Input />
-      </Form.Item>
+        <Form.Item<FieldType>
+          label="Username"
+          name="username"
+          rules={[{ required: true, message: "You need a username." }]}
+        >
+          <Input />
+        </Form.Item>
 
-      <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
-        <Button type="primary" htmlType="submit">
-          Done
-        </Button>
-      </Form.Item>
-    </Form>
+        <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+          <Button type="primary" htmlType="submit">
+            Done
+          </Button>
+        </Form.Item>
+      </Form>
+    </div>
   );
 };
 
