5db6a321f86bbe53c6abc3d6240ade1b3a35f232
Alastair7 <alexandru.ban.drmer@gmail.com>
2024-10-27 15:11:03 +0100

    STIMATE: Add more CSS to homepage

--- stimate-client/src/Pages/Home/HomePage.tsx
+++ stimate-client/src/Pages/Home/HomePage.tsx
@@ -1,10 +1,11 @@
+import { Button } from "antd";
 import { useState } from "react";
 import { UsernameForm } from "../../Features/UsernameForm/UsernameForm";
 import { getSessionUsername, hasSessionUsername } from "./domain";
-
+import "./homepage.css";
 const HomePage = () => {
   const [showUsernameForm, setShowUsernameForm] = useState(hasSessionUsername);
-  const nickname = showUsernameForm ? "" : getSessionUsername() ?? "";
+  const nickname = showUsernameForm ? "Stranger" : getSessionUsername() ?? "";
 
   return (
     <div>
@@ -12,7 +13,15 @@ const HomePage = () => {
         <UsernameForm setShowUsernameForm={setShowUsernameForm} />
       ) : null}
 
-      <h1>Hello, {nickname}</h1>
+      <div className="homepage_header__wrapper">
+        <h1>STIMATE</h1>
+        <h2>Hello, {nickname}! Are you ready to estimate?</h2>
+      </div>
+
+      <div className="homepage_buttons__wrapper">
+        <Button type="primary">Create Session</Button>
+        <Button>Join Session</Button>
+      </div>
     </div>
   );
 };
